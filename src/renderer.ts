2e402b013f0e2c6c3b987633be85f1cfa57b7e6a
SjorsHoek <S.hoek@geogap.nl>
2020-07-03 16:00:02 +0200

    remove more errors

--- src/renderer.ts
+++ src/renderer.ts
@@ -49,22 +49,22 @@ export class Renderer {
      * has no effect on serialised output, but allows it to verify render
      * quality.
      */
-    function injectBaseHref(origin: string) {
-      const base = document.createElement('base');
-      base.setAttribute('href', origin);
+    // function injectBaseHref(origin: string) {
+    //   const base = document.createElement('base');
+    //   base.setAttribute('href', origin);
 
-      const bases = document.head.querySelectorAll('base');
-      if (bases.length) {
-        // Patch existing <base> if it is relative.
-        const existingBase = bases[0].getAttribute('href') || '';
-        if (existingBase.startsWith('/')) {
-          bases[0].setAttribute('href', origin + existingBase);
-        }
-      } else {
-        // Only inject <base> if it doesn't already exist.
-        document.head.insertAdjacentElement('afterbegin', base);
-      }
-    }
+    //   const bases = document.head.querySelectorAll('base');
+    //   if (bases.length) {
+    //     // Patch existing <base> if it is relative.
+    //     const existingBase = bases[0].getAttribute('href') || '';
+    //     if (existingBase.startsWith('/')) {
+    //       bases[0].setAttribute('href', origin + existingBase);
+    //     }
+    //   } else {
+    //     // Only inject <base> if it doesn't already exist.
+    //     document.head.insertAdjacentElement('afterbegin', base);
+    //   }
+    // }
 
     const page = await this.browser.newPage();
 
